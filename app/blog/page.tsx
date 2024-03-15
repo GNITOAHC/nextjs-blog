fdfe6579c7e37a0ed4583fe48e69d421fa57b66e
GINTOAHC <chaotingchen10@gmail.com>
2024-03-15 16:46:32 +0800

    feat: Add search function

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,12 @@
+'use client'
 import Link from 'next/link'
-import { compareDesc, format, parseISO } from 'date-fns'
-import { getBlogPosts } from '@/lib/db/blog'
+import { format, parseISO } from 'date-fns'
 import type { Post } from 'blog'
 import React from 'react'
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
+import { Button } from '@/components/ui/button'
 
-function PostCard(post: Post) {
+function PostCard({ post }: { post: Post }) {
   return (
     <div className="mb-8">
       <h2 className="mb-1 text-xl">
@@ -29,34 +31,66 @@ function PostCard(post: Post) {
   )
 }
 
-function PostList() {
-  const posts = getBlogPosts().sort((a, b) =>
-    compareDesc(
-      new Date(a.metadata.lastEdit ?? a.metadata.date),
-      new Date(b.metadata.lastEdit ?? b.metadata.date)
-    )
-  )
-
+function PostList({ posts, query }: { posts: Post[]; query: string }) {
+  const filter = query != '' ? true : false
+  const filterFunc = (post: Post) => {
+    if (!filter) return true
+    if (post.metadata.title.toLowerCase().includes(query.toLowerCase())) {
+      return true
+    }
+    if (post.metadata.summary.toLowerCase().includes(query.toLowerCase())) {
+      return true
+    }
+    return false
+  }
   return (
     <div className="mx-auto max-w-xl py-8">
-      {/* <h1 className="mb-8 text-center text-2xl font-black"> */}
-      {/*   Next.js + Contentlayer Example */}
-      {/* </h1> */}
-      {posts.map((post, idx) => (
-        <PostCard key={idx} {...post} />
+      {posts.filter(filterFunc).map((post, idx) => (
+        <PostCard key={idx} post={post} />
       ))}
     </div>
   )
 }
 
 export default function Home() {
+  const [posts, setPosts] = React.useState<Post[]>([])
+  const [showInput, setShowInput] = React.useState<boolean>(false)
+  const [query, setQuery] = React.useState<string>('')
+
+  React.useEffect(() => {
+    async function fetchPosts() {
+      const res = await fetch('/api/posts', {
+        method: 'GET',
+      })
+      const postJson = await res.json()
+      setPosts(postJson as Post[])
+    }
+    fetchPosts()
+  }, [])
+
+  const toggle = () => {
+    setShowInput(showInput ? false : true)
+  }
+
   return (
     <main className="h-full relative overflow-y-hidden">
-      <h1 className="p-8 text-center text-2xl text-white sticky top-0">
-        Welcome to my blog
-      </h1>
+      <div className="flex flex-row justify-center items-center">
+        <h1 className="p-8 text-center text-2xl text-white sticky top-0">
+          Welcome to my blog
+        </h1>
+        <Button variant="ghost" size="icon" onClick={() => toggle()}>
+          <MagnifyingGlassIcon />
+        </Button>
+        {showInput && (
+          <input
+            type="text"
+            onChange={(e) => setQuery(e.target.value)}
+            className="max-w-xs bg-secondary rounded-md focus:outline-none p-1"
+          />
+        )}
+      </div>
       <div className="flex flex-col bottom-0 py-8 h-[calc(100%-6rem)] items-center overflow-y-scroll">
-        <PostList />
+        <PostList posts={posts} query={query} />
       </div>
     </main>
   )
