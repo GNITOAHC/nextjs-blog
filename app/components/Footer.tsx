522d65488e2346520e517ff2af44f33b87433f0b
GINTOAHC <chaotingchen10@gmail.com>
2024-08-14 20:25:02 +0800

    feat: /contact page

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 export function Footer() {
   return (
     <footer className="py-8">
-      <nav className="mb-3">
+      <nav className="mb-3 flex flex-row gap-x-3">
         <a href="/tags" className="hover:text-accent">
           Tags
+        </a>
+        <a href="/contact" className="hover:text-accent">
+          Contact
         </a>
       </nav>
       <div className="flex items-center justify-between">
